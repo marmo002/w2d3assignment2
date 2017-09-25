39dd7404b9aaaf40c923a3e8c5f511cccdc8aec1
marmo002 <marmo_02@hotmail.com>
2017-09-24 21:42:31 -0400

    Exercise3 done

--- exercises.rb
+++ exercises.rb
@@ -1,7 +1,7 @@
 #exercise 0
 #arrays and hashes
 fav_colours = ["red", "black", "blue", "orange"]
-ages = [28, 26, 52, 48 ]
+ages = ["28", "26", "52", "48" ]
 flip_coin = ['true', 'true', 'true', 'false', 'false']
 performing_artists = ['Will Smith', 'Denzel Washington', 'Emma Watson']
 favorites_colours = [:red, :black, :blue, :orange]
@@ -33,21 +33,34 @@ names_ages  = {
 
 #exercise 1
 
-print flip_coin
-print fav_colours[0]
-print ages.sort
+puts flip_coin
+puts fav_colours[0]
+puts ages.sort
 ages.push("0")
-print ages
-print movies[:The_Godfather]
+puts ages
+puts movies[:The_Godfather]
 
-#exercise 1
-print fav_colours[-1]
+#exercise 2
+puts fav_colours[-1]
 world_cities[:Bogota] = "3,893,127"
-print world_cities
+puts world_cities
 flip_coin.reverse!
-print flip_coin
-print world_cities[:Lima]
+puts flip_coin
+puts world_cities[:Lima]
 
 performing_artists.each do |artist|
   puts "#{artist} is the greatest ever!!"
 end
+
+#exercise 3
+puts performing_artists[0..1] #prints first and second element of arrays
+
+movies.each do |keys, values|
+  puts "#{keys} was succesfully realesed in #{values}"
+end
+
+puts ages = ages.sort.reverse
+puts ages
+
+movies[:The_Beauty_and_the_Beast] = "The movie was realesed both in 1991 and in 2017"
+puts movies
