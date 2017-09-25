10c1bb7755545ff893e8890cf9064b3efbe123e5
marmo002 <marmo_02@hotmail.com>
2017-09-24 22:27:46 -0400

    finish exercise 4

--- exercises.rb
+++ exercises.rb
@@ -2,7 +2,7 @@
 #arrays and hashes
 fav_colours = ["red", "black", "blue", "orange"]
 ages = ["28", "26", "52", "48" ]
-flip_coin = ['true', 'true', 'true', 'false', 'false']
+flip_coin = ['true', 'true', 'true', 'false', 'false']#heads = true
 performing_artists = ['Will Smith', 'Denzel Washington', 'Emma Watson']
 favorites_colours = [:red, :black, :blue, :orange]
 
@@ -28,7 +28,8 @@ world_cities  = {
 names_ages  = {
   :Luis => "26",
   :Alberto => "22",
-  :Cludio => "20"
+  :Claudio => "20",
+  :Jose => "52"
 }
 
 #exercise 1
@@ -64,3 +65,24 @@ puts ages
 
 movies[:The_Beauty_and_the_Beast] = "The movie was realesed both in 1991 and in 2017"
 puts movies
+
+#exercise 4
+youngsters = names_ages.select do |names, ages|
+  ages < "30"
+end
+puts "This are my youngsters: "
+youngsters.each do |name, age|
+  puts " - #{name} is #{age} years old"
+end
+
+aged = names_ages.select do |_names, _ages|
+  _ages > "40"
+end
+puts "This are my oldest friends: "
+aged.each do |_name, _age|
+  puts " - #{_name} is #{_age} years old"
+end
+
+puts "I flip heads #{flip_coin.count("true")} times"
+
+puts "New york city now has #{names_ages[:New_York] = "12,000"} people"
