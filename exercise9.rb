97752ce7b95b5225c7063862f57145aa042d5c91
marmo002 <marmo_02@hotmail.com>
2017-09-25 20:36:18 -0400

    finish exercise 9

--- exercise9.rb
+++ exercise9.rb
@@ -1,17 +1,18 @@
 students = {
-  :cohort1 => 34,
-  :cohort2 => 42,
-  :cohort3 => 22
+  "cohort1" => 34,
+  "cohort2" => 42,
+  "cohort3" => 22
 }
 
-def students_cohort(students)
+def display(students)
   students.each do |keys, values|
-    puts "#{keys}: #{values} students"
+    puts " -In #{keys} there are #{values}"
   end
 end
-students_cohort(students)
+puts "Number of students:"
+display(students)
 
-students[:cohort4] = 43
+students["cohort4"] = 43
 
 def keys(students)
   students.keys
@@ -23,5 +24,26 @@ def increase_by_five(students)
     students[key] = value*1.05
   end
 end
-
 puts increase_by_five(students)
+
+
+def delete_cohort(aray, cohort_name)
+  aray.delete(cohort_name)
+end
+delete_cohort(students, "cohort2")
+puts students
+
+def total_students(aray)
+  sum = 0
+  aray.each { |c_name, students_count| sum += students_count }
+  return sum
+end
+puts "Total number of students: #{total_students(students)}"
+
+staff = {
+  "cohort1" => 5,
+  "cohort2" => 4,
+  "cohort3" => 6
+}
+puts "Staff:"
+display(staff)
